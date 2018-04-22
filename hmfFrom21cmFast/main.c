98c72c084fd23ecc6278666a5c7d01f036f1ede9
Carmelo Evoli <carmelo.evoli@me.com>
2018-04-22 11:28:14 +0200

    add hmf calculations

---
+++ hmfFrom21cmFast/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include "Parameter_files/INIT_PARAMS.H"
+#include "Parameter_files/ANAL_PARAMS.H"
+#include "Parameter_files/HEAT_PARAMS.H"
+
+#define POW2(A) ((A)*(A))
+#define POW3(A) ((A)*(A)*(A))
+
+int main(int argc, char ** argv)
+{   
+    init_ps();
+    
+    /*
+     FUNCTION dNdM(z, M)
+     Computes the Press_schechter mass function with Sheth-Torman correction for ellipsoidal collapse at
+     redshift z, and dark matter halo mass M (in solar masses).
+     
+     The return value is the number density per unit mass of halos in the mass range M to M+dM in units of:
+     comoving Mpc^-3 Msun^-1
+     
+     Reference: Sheth, Mo, Torman 2001
+     */
+    
+    double z = 10;
+    double dNdM = -1;
+    
+    printf ("#Redshift - Mass [Msun] - dNdM [Mpc^-3 Msun^-1]\n");
+    
+    for (double M = 1e6; M < 1e11; M *= 1.1){
+        dNdM = dNdM_st(z,M);
+        printf ("%5.2e %5.2e %5.2e\n", z, M, dNdM);
+    }
+    
+    return 0;
+}
